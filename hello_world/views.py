c5c34f2279789a89f9fcabce485366888377c183
Your Name <your-email@example.com>
2025-12-15 13:25:29 +0000

    Add home view and wire root URL

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    if request.method == "POST":
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
+    return HttpResponse("Hello, world!")
+
+def home(request):
+    return HttpResponse("Welcome to the home page")
 
